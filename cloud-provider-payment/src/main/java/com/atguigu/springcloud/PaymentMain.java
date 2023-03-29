09eb04f929dbf92de3e33eaea641ee9463d3db67
yiyuan <1187998300@qq.com>
2023-03-30 00:17:39 +0800

    gateway静态路由

--- cloud-provider-payment/src/main/java/com/atguigu/springcloud/PaymentMain.java
+++ cloud-provider-payment/src/main/java/com/atguigu/springcloud/PaymentMain.java
@@ -1,9 +1,12 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,19 @@ public class PaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class,args);
     }
+    @Bean  //用于注册Servlet
+    public ServletRegistrationBean getServlet() {
+        //用于抓取当前服务的数据流（hystrix的数据流）
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        用于注册Servlet
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        //设置Servlet加载时机
+        registrationBean.setLoadOnStartup(1);
+        //映射路径
+        registrationBean.addUrlMappings("/hystrix.stream");
+        //Servlet名称
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 }
